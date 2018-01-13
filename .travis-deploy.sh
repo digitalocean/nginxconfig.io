4581369f819563e05cd4bec2095d4cd5fe6626ca
Szekeres Bálint <szekeres.balint@hps.hu>
2018-01-13 13:01:07 +0100

    Travis StrictHostKeyChecking no

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)"
 base64 --decode <<< "$DEPLOY_PRIVATE_KEY" > deploy_key
 chmod 600 deploy_key
 ssh-add deploy_key
-ssh -l root szekeres.me \
+ssh -o "StrictHostKeyChecking no" -l root szekeres.me \
 	"sudo -u www-data -H sh -c ' \
 	cd /var/www/nginxconfig.io; \
 	git pull origin master'"
