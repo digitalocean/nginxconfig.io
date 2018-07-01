c43ec344fd85178ba7b49b4d173dc718855a08f6
Szekeres Bálint <szekeres.balint@hps.hu>
2018-07-01 15:28:35 +0200

    cypress updates

--- cypress/integration/nginxconfig_spec.js
+++ cypress/integration/nginxconfig_spec.js
@@ -1,21 +1,19 @@
-const path = require('path')
-const root = 'http://localhost:8080'
+const path = require('path');
+const root = 'http://localhost:8080';
 
-describe('nginx.config.io', function () {
+describe('nginxconfig.io', function () {
 
 	beforeEach(function () {
-		cy.visit(root)
-	})
+		cy.visit(root);
+	});
 
 	it('<title> is correct', function () {
-		// https://on.cypress.io/visit
-		cy.title().should('include', 'nginxconfig.io')
-	})
+		cy.title().should('include', 'nginxconfig.io');
+	});
 
 	it('should use passed params', function () {
-		cy.visit(root + '?domain=mydomain.fr')
-		cy.get('input.domain').should('have.value', 'mydomain.fr')
-		cy.get('#file-nginx').contains('server_name mydomain.fr;')
-	})
-
-})
+		cy.visit(root + '?domain=nginxconfig.io');
+		cy.get('input.domain').should('have.value', 'nginxconfig.io');
+		cy.get('#file-nginx').contains('server_name nginxconfig.io;');
+	});
+});
