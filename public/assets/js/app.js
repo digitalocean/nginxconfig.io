57aa31826a347e48eeb018a2cbb2065ee0a00235
Szekeres Bálint <szekeres.balint@hps.hu>
2018-09-30 18:03:12 +0200

    more extensions

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -105,12 +105,15 @@
 
 		$scope.extensions = {
 			assets:	'css(\\.map)?|js(\\.map)?',
-			fonts:	'ttf|ttc|otf|eot|woff|woff2',
+			fonts:	'ttf|ttc|otf|eot|woff2?',
 			svg:	'svgz?',
 			images:	'jpe?g|png|gif|ico|cur|heic|webp|tiff?',
 			audio:	'mp3|m4a|aac|ogg|midi?|wav',
 			video:	'mp4|mov|webm|mpe?g|avi|ogv|flv|wmv',
-			docs:	'pdf|docx?|xlsx?|pptx?'
+			docs:	'pdf|' +
+					'docx?|dotx?|docm|dotm|' +
+					'xlsx?|xltx?|xlsm|xltm|' +
+					'pptx?|potx?|pptm|potm|ppsx?'
 		};
 
 		$scope.gzipTypes = 'text/plain text/css text/xml application/json application/javascript application/xml+rss application/atom+xml image/svg+xml';
