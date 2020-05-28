19cac71df07d8c5b0425c5ee31402c68ebf7870a
MattIPv4 <me@mattcowley.co.uk>
2020-05-28 16:50:03 +0100

    Add in all the missing copyright headers

--- src/nginxconfig/build/template.js
+++ src/nginxconfig/build/template.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 const path = require('path');
 const fs = require('fs');
 
@@ -5,9 +21,11 @@ const fs = require('fs');
 const main = () => {
     const buildDir = path.join(__dirname, '..', '..', '..', 'build');
     let template = fs.readFileSync(path.join(buildDir, 'base.html'), 'utf8');
+
     template = template.replace('<block name="title"></block>', 'NGINXConfig | ');
     template = template.replace('<block name="head"></block>', '');
     template = template.replace('<block name="content"></block>', '<div id="app"></div>');
+
     fs.writeFileSync(path.join(buildDir, 'index.html'), template);
 };
 
