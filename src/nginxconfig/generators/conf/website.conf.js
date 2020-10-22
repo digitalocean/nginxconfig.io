f460423808dccc037b16f5d577edb06f03813777
Faiz Azhar <gabanz@gmail.com>
2020-10-23 00:23:08 +0800

    Update src/nginxconfig/generators/conf/website.conf.js
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>

--- src/nginxconfig/generators/conf/website.conf.js
+++ src/nginxconfig/generators/conf/website.conf.js
@@ -173,7 +173,7 @@ export default (domain, domains, global) => {
         if (domain.logging.accessLog.computed)
             serverConfig.push(['access_log', getAccessLogDomainPath(domain, global) + (global.logging.cloudflare.computed ? ' cloudflare' : '')]);
         if (domain.logging.errorLog.computed)
-            serverConfig.push(['error_log', getErrorLogDomainPath(domain, global)]); 
+            serverConfig.push(['error_log', getErrorLogDomainPath(domain, global)]);
     }
 
     // index.php
