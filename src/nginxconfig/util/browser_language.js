22a071d9e24e13d7810e0a4644856f92a65a729e
墨娘 <30471441@qq.com>
2020-12-16 14:24:00 +0800

    Update unit testing

--- src/nginxconfig/util/browser_language.js
+++ src/nginxconfig/util/browser_language.js
@@ -1,20 +1,16 @@
 /*
 Copyright 2020 DigitalOcean
-
 This code is licensed under the MIT License.
 You may obtain a copy of the License at
 https://github.com/digitalocean/nginxconfig.io/blob/master/LICENSE or https://mit-license.org/
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions :
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
@@ -38,7 +34,8 @@ export default () => {
         if (typeof window.navigator.language === 'string')
             userLocales.add(window.navigator.language);
         if (Intl && 'DateTimeFormat' in Intl)
-            userLocales.add(Intl.DateTimeFormat().resolvedOptions().locale);
+            if (Intl.DateTimeFormat().resolvedOptions().locale !== 'und')
+                userLocales.add(Intl.DateTimeFormat().resolvedOptions().locale);
 
         // Try to find an exact region/language match
         const i18nPackLocales = Object.keys(i18nPacks);
@@ -53,7 +50,11 @@ export default () => {
         }, {});
 
         // Try to match a user language to a pack language
-        const langMatch = [...userLocales.values()].find(x => i18nPackLanguages.includes(x.split('-')[0].toLowerCase()));
-        if (langMatch) return i18nPackLanguages[langMatch.split('-')[0].toLowerCase()];
+        if (i18nPackLanguages){
+            const langMatch = [...userLocales.values()].find(x => Object.keys(i18nPackLanguages).includes(x.split('-')[0].toLowerCase()));
+            if (langMatch) return i18nPackLanguages[langMatch.split('-')[0].toLowerCase()];
+        }
+
+        return false;
     }
 };
--- test/testBrowserLanguage.js
+++ test/testBrowserLanguage.js
@@ -1,33 +1,112 @@
 import browserLanguage from '../src/nginxconfig/util/browser_language';
 
-let languages = [];
+class mockLocales{
+    static languages = [];
 
-Object.defineProperty(window.navigator,'languages',{get:function(){
-    return languages;
+    static language = null;
+    IntlBackup = null;
+
+    setNavigatorLanguages(langs){
+        mockLocales.languages = langs;
+        return this;
+    }
+
+    setNavigatorLanguage(lang){
+        mockLocales.language = lang;
+        return this;
+    }
+
+    setDateTimeLocale(locale) {
+        const newDateTimeForMat = new Intl.DateTimeFormat(locale);
+        this.IntlBackup = Intl;
+        if (!locale){
+            // eslint-disable-next-line no-global-assign
+            Intl = undefined;
+            return;
+        }
+
+        // eslint-disable-next-line no-global-assign
+        Intl = {
+            DateTimeFormat(){
+                return newDateTimeForMat;
+            },
+        };
+        return this;
+    }
+
+    restoreDateTimeLocale(){
+        // eslint-disable-next-line no-global-assign
+        Intl = this.IntlBackup;
+        return this;
+    }
+}
+Object.defineProperty(window.navigator,'languages',{get:() => {
+    return mockLocales.languages ?? [];
 }});
 
-Object.defineProperty(window.navigator,'language',{get:function(){
-        return languages[0];
+Object.defineProperty(window.navigator,'language',{get:() => {
+    return mockLocales.language ?? null;
 }});
 
-const setNavigatorLanguges = lang => { languages = lang; };
 
-// eslint-disable-next-line no-global-assign
-Intl = undefined;
+describe('#testGetBrowserLanguage', ()=> {
+    const Locale = new mockLocales();
 
-describe('#testGetBrowserLanguage', function() {
-    test('#inChina', function() {
-        setNavigatorLanguges(['zh-CN', 'zh', 'en-US', 'en']);
+    test('#inChina', ()=> {
+        Locale.setNavigatorLanguages(['zh-CN', 'zh', 'en-US', 'en']);
+        Locale.setDateTimeLocale(undefined);
         expect(browserLanguage()).toEqual('zhCN');
+        Locale.restoreDateTimeLocale();
     });
 
-    test('#inHongKong',function (){
-        setNavigatorLanguges(['zh-TW','zh','en-US','en']);
+    test('#inHongKong',()=> {
+        Locale.setNavigatorLanguages(['zh-TW','zh','en-US','en']);
+        Locale.setDateTimeLocale(undefined);
         expect(browserLanguage()).toEqual('zhTW');
+        Locale.restoreDateTimeLocale();
     });
 
-    test('#inJapan',function(){
-        setNavigatorLanguges(['ja-JP','ja']);
+    test('#inJapan',()=> {
+        Locale.setNavigatorLanguages(['ja-JP','ja']);
+        Locale.setDateTimeLocale(undefined);
         expect(browserLanguage()).toBeFalsy();
+        Locale.restoreDateTimeLocale();
+    });
+});
+
+describe('#test different sources',()=>{
+    const Locale = new mockLocales();
+
+    test('#navigator and locale is undefined',()=>{
+        Locale.setNavigatorLanguages(undefined);
+        Locale.setNavigatorLanguage(undefined);
+        Locale.setDateTimeLocale(undefined);
+        expect(browserLanguage()).toBeFalsy();
+        Locale.restoreDateTimeLocale();
+    });
+
+    test('#language as en and languages is undefined',()=>{
+        Locale.setNavigatorLanguage('en');
+        Locale.setNavigatorLanguages(undefined);
+        Locale.setDateTimeLocale(undefined);
+        expect(browserLanguage()).toEqual('en');
+        Locale.restoreDateTimeLocale();
+    });
+
+    test('#language is undefined and languages is en-US,en and Intl is undefined',()=>{
+        Locale.setNavigatorLanguage(undefined);
+        Locale.setNavigatorLanguages(['en-US','en']);
+        Locale.setDateTimeLocale(undefined);
+        expect(browserLanguage()).toEqual('en');
+        Locale.restoreDateTimeLocale();
+    });
+
+    test('#navigator is undefined and locale is en-US',()=>{
+        const oldNavigator = window.navigator;
+        window.navigator = undefined;
+        Locale.setDateTimeLocale('en-US');
+        expect(browserLanguage()).toEqual('en');
+        Locale.restoreDateTimeLocale();
+        window.navigator = oldNavigator;
     });
 });
