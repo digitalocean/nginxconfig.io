f63a6fe979a0652fe1763ffb237ef491da9afd86
墨娘 <30471441@qq.com>
2020-12-29 14:39:36 +0800

    Translate the new phrase

--- src/nginxconfig/i18n/zh-tw/templates/domain_sections/routing.js
+++ src/nginxconfig/i18n/zh-tw/templates/domain_sections/routing.js
@@ -27,8 +27,8 @@ THE SOFTWARE.
 import common from '../../common';
 
 export default {
-    fallbackRouting: '後備路由',
-    fallbackRoutingPhpPath: `後備路由${common.php}路徑`,
+    fallbackRouting: '回調路由',
+    fallbackRoutingPhpPath: `回調路由${common.php}路徑`,
     legacyPhpRouting: `傳統${common.php}路由`,
     enableLegacyRouting: `${common.enable}傳統路由`,
     routing: '路由設定',
