189a0fc3cf466943f4565e4eefb1a6494d50b0ec
Atila Silva <atila.danvi@outlook.com>
2021-02-10 13:11:22 -0300

    Prevent access error in phpServerOptions (#221)
    * Prevent access error in phpServerOptions
    
    * fix missing semicolon

--- src/nginxconfig/templates/global_sections/php.vue
+++ src/nginxconfig/templates/global_sections/php.vue
@@ -215,6 +215,8 @@ THE SOFTWARE.
             },
             // Ensure 'Custom'/'Disabled' get translated in VueSelect on language switch
             '$i18n.locale'() {
+                if (!this.$refs.phpServerOptions)
+                    return false;
                 const updated = this.phpServerOptions
                     .find(x => x.value === this.$refs.phpServerOptions.$data._value.value);
                 if (updated) this.$refs.phpServerOptions.$data._value = updated;
