90afc38a3871287a39ebe46c8f8fec355811e9bf
powerdot <donpsix@gmail.com>
2021-02-19 20:15:17 +0300

    Updates to RU langauge pack (#231)
    * Russian lang
    
    * Russian Lang
    
    * Update src/nginxconfig/i18n/fr/languages.js
    
    Mama Mia! Pizza!
    
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>
    
    * Update src/nginxconfig/i18n/ru/common.js
    
    Tabs to 4 spaces
    
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>
    
    * Matt language revision
    
    2020 -> 2021
    Added translations
    
    * Little updates
    
    1 translation
    1 correction
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Update https.js
    
    Broken French
    
    * Translation fix
    
    Better to UI, better to understand, 4 mistakes fixed
    
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>

--- src/nginxconfig/i18n/ru/templates/global_sections/python.js
+++ src/nginxconfig/i18n/ru/templates/global_sections/python.js
@@ -28,5 +28,5 @@ import common from '../../common';
 
 export default {
     pythonServer: `${common.python} сервер`,
-    pythonMustBeEnabledOnOneSite: `${common.python} должен быть включен как минимум на одном сайте, чтобы сконфигурировать глобаьные настройки ${common.python}.`,
+    pythonMustBeEnabledOnOneSite: `${common.python} должен быть включен как минимум на одном сайте, чтобы сконфигурировать глобальные настройки ${common.python}.`,
 };
