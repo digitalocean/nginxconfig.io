c3cf3d857487866d671dee1a1f002694bb09cdeb
Valery Kovalev <v.kovalev@reg.ru>
2021-04-27 15:53:57 +0300

    #85773 imports update

--- src/nginxconfig/mount.js
+++ src/nginxconfig/mount.js
@@ -37,7 +37,7 @@ import './scss/style.scss';
 import Vue from 'vue';
 import './util/prism_bundle';
 import { i18n } from './i18n/setup';
-import App from './templates/app';
+import App from './templates/app.vue';
 
 // Run the app
 new Vue({
