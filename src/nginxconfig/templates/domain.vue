78dfaa858b1b8392c2c5d55cc3e0e880a71509ea
MattIPv4 <me@mattcowley.co.uk>
2021-05-06 19:16:11 +0100

    Add unstyled warning to onion tab

--- src/nginxconfig/templates/domain.vue
+++ src/nginxconfig/templates/domain.vue
@@ -34,7 +34,10 @@ THE SOFTWARE.
             <div class="tabs">
                 <ul>
                     <li v-for="tab in tabs" :class="tabClass(tab.key)">
-                        <a @click="showTab(tab.key)">{{ $t(tab.display) }}{{ changes(tab.key) }}</a>
+                        <a @click="showTab(tab.key)">
+                            {{ $t(tab.display) }}{{ changes(tab.key) }}
+                            <i v-if="warnings(tab.key)" class="fas fa-exclamation-triangle"></i>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -42,6 +45,7 @@ THE SOFTWARE.
             <component :is="tab"
                        v-for="tab in tabs"
                        :key="tab.key"
+                       :ref="tab.key"
                        :data="$props.data[tab.key]"
                        :style="{ display: active === tab.key ? undefined : 'none' }"
                        class="container"
@@ -112,6 +116,10 @@ THE SOFTWARE.
                 const changes = this.changesCount(tab);
                 if (changes) return ` (${changes.toLocaleString()})`;
                 return '';
+            },
+            warnings(tab) {
+                if (!Object.prototype.hasOwnProperty.call(this.$refs, tab)) return false;
+                return this.$refs[tab][0].hasWarnings || false;
             },
             setValue(tab, key, val) {
                 Object.assign(this.$props.data[tab][key], { value: val, computed: val });
