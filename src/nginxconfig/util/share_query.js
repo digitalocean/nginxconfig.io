31295377a9d8c1a28d253adf05b9eaa5bdf573a1
MattIPv4 <me@mattcowley.co.uk>
2021-05-06 15:28:25 +0100

    Reduce the query param cut-off to 1000 chars

--- src/nginxconfig/util/share_query.js
+++ src/nginxconfig/util/share_query.js
@@ -30,5 +30,5 @@ import exportData from './export_data';
 export default (domains, global) => {
     const data = exportData(domains, global);
     const query = qs.stringify(data, { allowDots: true });
-    return `${query.length > 4000 ? '#' : ''}${query.length ? '?' : ''}${query}`;
+    return `${query.length > 1000 ? '#' : ''}${query.length ? '?' : ''}${query}`;
 };
