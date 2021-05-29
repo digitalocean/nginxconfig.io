d2feccc12199f6766d6162ed6fa9318bda111db7
Yasio <baszczok@yasio.pl>
2021-05-29 16:17:57 +0200

    add new string translations to Polish lang

--- src/nginxconfig/i18n/pl/templates/global_sections/reverse_proxy.js
+++ src/nginxconfig/i18n/pl/templates/global_sections/reverse_proxy.js
@@ -26,7 +26,11 @@ THE SOFTWARE.
 
 import common from '../../common';
 
+const legacyXForwarded = 'nagłówka X-Forwarded-* starego typu';
+
 export default {
     reverseProxyMustBeEnabledOnOneSite: `${common.reverseProxy} musi być włączony na conajmniej jednej stronie, aby móc zastosować globalną konfigurację ${common.reverseProxyLower}.`,
     seconds: 'sekund(y)',
+    passOn: `Przekazywanie ${legacyXForwarded}`,
+    remove: `Aktywne usuwanie ${legacyXForwarded}`,
 };
--- src/nginxconfig/i18n/pl/templates/global_sections/security.js
+++ src/nginxconfig/i18n/pl/templates/global_sections/security.js
@@ -27,6 +27,6 @@ THE SOFTWARE.
 import common from '../../common';
 
 export default {
-    whenUsingWordPressUnsafeEvalIsOftenRequiredToAllowFunctionality: `Korzystając z ${common.wordPress}, <code class="slim">'unsafe-eval'</code> jest często wymagany w Content Security Policy aby panel administracyjny działał poprawnie.`,
+    whenUsingWordPressUnsafeEvalIsOftenRequiredToAllowFunctionality: `Korzystając z ${common.wordPress}, <code class="slim">script-src 'self' 'unsafe-inline' 'unsafe-eval';</code> jest często wymagany w Content Security Policy aby panel administracyjny działał poprawnie.`,
     security: 'Bezpieczeństwo',
 };
