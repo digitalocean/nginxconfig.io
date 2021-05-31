f9fb25c9417f19b7b121b98724e47fdb9def3aff
sve1r <plekiyeon@gmail.com>
2021-05-31 21:15:55 +0800

    Add security translation for Simplified Chinese

--- src/nginxconfig/i18n/zh-cn/templates/global_sections/security.js
+++ src/nginxconfig/i18n/zh-cn/templates/global_sections/security.js
@@ -27,6 +27,6 @@ THE SOFTWARE.
 import common from '../../common';
 
 export default {
-    whenUsingWordPressUnsafeEvalIsOftenRequiredToAllowFunctionality: `当使用${common.wordPress}时，<code class="slim">script-src 'self' 'unsafe-inline' 'unsafe-eval';</code> 通常需要置于 Content-Security-Policy 内容安全策略中，以确保管理面板的正常运行。`,
+    whenUsingWordPressUnsafeEvalIsOftenRequiredToAllowFunctionality: `当使用${common.wordPress}时，<code class="slim">script-src 'self' 'unsafe-inline' 'unsafe-eval';</code> 通常需要置于内容安全策略中，以确保管理面板的正常运行。`,
     security: '安全',
 };
