4a613a389750a7fcf905a28dcbde35a630c7101e
sve1r <plekiyeon@gmail.com>
2021-05-31 21:02:00 +0800

    Add performance translation for Simplified Chinese

--- src/nginxconfig/i18n/zh-cn/templates/global_sections/performance.js
+++ src/nginxconfig/i18n/zh-cn/templates/global_sections/performance.js
@@ -30,10 +30,10 @@ export default {
     gzipCompression: 'Gzip 压缩',
     enableGzipCompression: `${common.enable}Gzip压缩`,
     brotliCompression: 'Brotli 压缩',
-    enableBrotliCompression: `${common.enable}brotli压缩`,
-    brotliIsANonStandardModule: 'Brotli isn\'t a standard NGINX module, check the ', // TODO: translate
-    brotliGoogleNgxBrotliProject: 'Google ngx_brotli project', // TODO: translate
-    brotliForBuildingNginxWithBrotli: ' for how to build NGINX with Brotli!', // TODO: translate
+    enableBrotliCompression: `${common.enable} Brotli 压缩`,
+    brotliIsANonStandardModule: 'Brotli 并不是一个 standard NGINX 模块, 请查看 ',
+    brotliGoogleNgxBrotliProject: 'Google ngx_brotli 项目',
+    brotliForBuildingNginxWithBrotli: ' 以构建支持 Brotli 的 NGINX!',
     expirationForAssets: '资源有效期',
     expirationForMedia: '媒体资源有效期',
     expirationForSvgs: 'SVGs有效期',
