7968dfe0aee1e7bc5d26e093fb97b122d1b3835e
Luca <lucatankard@gmail.com>
2021-10-04 11:53:05 +0100

    fix: Hyphenate
    Co-authored-by: Matt (IPv4) Cowley <me@mattcowley.co.uk>

--- src/nginxconfig/i18n/en/templates/app.js
+++ src/nginxconfig/i18n/en/templates/app.js
@@ -30,7 +30,7 @@ export default {
     title: `${common.nginx}Config`,
     description: `The easiest way to configure a performant, secure, and stable ${common.nginx} server.`,
     singleColumnMode: 'Single column mode',
-    splitColumnMode: 'Side by side mode',
+    splitColumnMode: 'Side-by-side mode',
     perWebsiteConfig: 'Per-website config',
     addSite: 'Add site',
     globalConfig: 'Global config',
