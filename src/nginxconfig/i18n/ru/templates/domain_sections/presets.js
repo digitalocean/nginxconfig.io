dae1173318d297a97159a21ed1de321432efe635
Муравьёв Семён <s01@ngs.ru>
2021-10-04 18:41:08 +0300

    Russian translation updates (#305)
    * Add Russian translations for HTTP3 words
    
    * Add Russian translations for Reuseport
    
    * Correct Фронтэнд to Фронтенд
    
    * Typo "приветсвуем" in Russian translation

--- src/nginxconfig/i18n/ru/templates/domain_sections/presets.js
+++ src/nginxconfig/i18n/ru/templates/domain_sections/presets.js
@@ -27,7 +27,7 @@ THE SOFTWARE.
 export default {
     presets: 'Пресеты',
     itLooksLikeYouCustomisedTheConfig: 'Похоже, вы уже настроили конфигурацию для этого домена. Выбор нового пресета может привести к сбросу или изменению некоторых настроек, которые Вы настроили ранее.',
-    frontend: 'Фронтэнд',
+    frontend: 'Фронтенд',
     nodeJs: 'Node.js',
     singlePageApplication: 'Одностраничное приложение',
 };
