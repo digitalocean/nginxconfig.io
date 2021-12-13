633963ce4328079b41f28ae6e9332a775743642a
Daniel Kreiseder <dani@pccp.at>
2021-12-13 11:31:00 +0100

    Improve german translation in setup instructions

--- src/nginxconfig/i18n/de/templates/setup_sections/certbot.js
+++ src/nginxconfig/i18n/de/templates/setup_sections/certbot.js
@@ -29,11 +29,11 @@ import common from '../../common';
 const certbot = 'Certbot';
 
 export default {
-    commentOutSslDirectivesInConfiguration: `Auskommentieren ${common.ssl}-relevanter Direktiven in deiner Konfiguration:`,
-    reloadYourNginxServer: `Reload deines ${common.nginx} Servers:`,
-    obtainSslCertificatesFromLetsEncrypt: `${common.ssl} Zertifikate von ${common.letsEncrypt} mittels ${certbot} erhalten:`,
-    uncommentSslDirectivesInConfiguration: `Einkommentieren ${common.ssl}-relevanter Direktiven in deiner Konfiguration:`,
-    configureCertbotToReloadNginxOnCertificateRenewal: `Konfiguriere ${certbot} um ${common.nginx} neu zu laden, wenn Zertifikate erfolgreich erneuert wurden:`,
+    commentOutSslDirectivesInConfiguration: `Kommentiere ${common.ssl}-relevante Direktiven in deiner Konfiguration aus:`,
+    reloadYourNginxServer: `Führe einen reload deines ${common.nginx} Server aus:`,
+    obtainSslCertificatesFromLetsEncrypt: `Erhalte ${common.ssl} Zertifikate von ${common.letsEncrypt} mittels ${certbot} :`,
+    uncommentSslDirectivesInConfiguration: `Kommentiere ${common.ssl}-relevante Direktiven in deiner Konfiguration ein:`,
+    configureCertbotToReloadNginxOnCertificateRenewal: `Konfiguriere ${certbot} um ${common.nginx} neu zu laden, wenn die Zertifikate erfolgreich erneuert wurden:`,
     certbotDoesNotNeedToBeSetupForYourConfiguration: `${certbot} muss für deine ${common.nginx} Konfiguration nicht eingerichtet werden.`,
     certbot,
 };
