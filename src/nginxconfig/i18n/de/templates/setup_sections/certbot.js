7aaf4a7424b01eed0669ae0e11f911b040c52dd7
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2021-12-16 15:00:08 +0000

    Update src/nginxconfig/i18n/de/templates/setup_sections/certbot.js

--- src/nginxconfig/i18n/de/templates/setup_sections/certbot.js
+++ src/nginxconfig/i18n/de/templates/setup_sections/certbot.js
@@ -31,7 +31,7 @@ const certbot = 'Certbot';
 export default {
     commentOutSslDirectivesInConfiguration: `Kommentiere ${common.ssl}-relevante Direktiven in deiner Konfiguration aus:`,
     reloadYourNginxServer: `Führe einen reload deines ${common.nginx} Server aus:`,
-    obtainSslCertificatesFromLetsEncrypt: `Erhalte ${common.ssl} Zertifikate von ${common.letsEncrypt} mittels ${certbot} :`,
+    obtainSslCertificatesFromLetsEncrypt: `Erhalte ${common.ssl} Zertifikate von ${common.letsEncrypt} mittels ${certbot}:`,
     uncommentSslDirectivesInConfiguration: `Kommentiere ${common.ssl}-relevante Direktiven in deiner Konfiguration ein:`,
     configureCertbotToReloadNginxOnCertificateRenewal: `Konfiguriere ${certbot} um ${common.nginx} neu zu laden, wenn die Zertifikate erfolgreich erneuert wurden:`,
     certbotDoesNotNeedToBeSetupForYourConfiguration: `${certbot} muss für deine ${common.nginx} Konfiguration nicht eingerichtet werden.`,
