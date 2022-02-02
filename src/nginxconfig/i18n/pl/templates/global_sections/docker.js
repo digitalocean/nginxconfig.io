cd4979ae7330b60c0808303fc0086fd7a4e8d838
Paweł pidpawel Kozubal <git@pidpawel.eu>
2022-02-02 18:07:14 +0100

    Fix typo in pl translation

--- src/nginxconfig/i18n/pl/templates/global_sections/docker.js
+++ src/nginxconfig/i18n/pl/templates/global_sections/docker.js
@@ -35,7 +35,7 @@ export default {
     dockerCompose: `${docker} Compose`,
     applyDockerTweaks: `Zastosuj poprawki pod ${docker}a`,
     applyDockerTweaksForNginx: `Zastosuj poprawki konfiguracyjne pod ${common.nginx} z ${docker}em`,
-    applyDockerTweaksExplainer: `Aktualizuje urzytkownika ${common.nginx} na <code class="slim">nginx</code> oraz ustawia pid na <code class="slim">/var/run/nginx.pid</code>`,
+    applyDockerTweaksExplainer: `Aktualizuje użytkownika ${common.nginx} na <code class="slim">nginx</code> oraz ustawia pid na <code class="slim">/var/run/nginx.pid</code>`,
     includeDockerfile: `Dołącz ${dockerfile}, aby uruchomić ${common.nginx} z ${docker}`,
     includeDockerCompose: `Dołącz docker-compose, aby uruchomić ${common.nginx} z docker-compose`,
 };
