75159ca9834d5f7089c8e3251e2e6621cbf097b6
MattIPv4 <me@mattcowley.co.uk>
2022-02-22 13:19:42 +0000

    Suppress logging with NODE_ENV

--- src/nginxconfig/templates/app.vue
+++ src/nginxconfig/templates/app.vue
@@ -134,6 +134,7 @@ THE SOFTWARE.
     import analytics from '../util/analytics';
     import browserLanguage from '../util/browser_language';
     import { defaultPack, availablePacks } from '../util/language_packs';
+    import { info, error } from '../util/log';
 
     import { setLanguagePack } from '../i18n/setup';
     import generators from '../generators';
@@ -243,7 +244,7 @@ THE SOFTWARE.
                     // Update the locale
                     setLanguagePack(data.computed).then(() => {
                         // Done
-                        console.log('Language set to', data.computed);
+                        info('Language set to', data.computed);
                         this.$data.languagePrevious = data.computed;
                         this.$data.languageLoading = false;
 
@@ -251,8 +252,7 @@ THE SOFTWARE.
                         this.languageSetEvent(!interactive);
                     }).catch((err) => {
                         // Error
-                        console.log('Failed to set language to', data.computed);
-                        console.error(err);
+                        error(`Failed to set language to ${data.computed}`, err);
 
                         // Fallback to last known good
                         data.value = this.$data.languagePrevious;
@@ -358,9 +358,9 @@ THE SOFTWARE.
                             file,
                         ];
                     });
-                } catch (e) {
+                } catch (err) {
                     // If diff generation goes wrong, don't show any diff
-                    console.error(e);
+                    error('Failed to compute and highlight diff', err);
                     this.$data.confFilesOutput = Object.entries(newConf).map(([ name, content ]) => {
                         const safeName = escape(name);
                         const safeContent = escape(content);
