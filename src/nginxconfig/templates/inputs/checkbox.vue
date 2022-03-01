585bdaf5549ea6805f42660770512204eede4298
MattIPv4 <me@mattcowley.co.uk>
2022-03-01 23:19:26 +0000

    Use patched pretty-checkbox-vue from do-vue

--- src/nginxconfig/templates/inputs/checkbox.vue
+++ src/nginxconfig/templates/inputs/checkbox.vue
@@ -25,13 +25,21 @@ THE SOFTWARE.
 -->
 
 <template>
-    <PrettyInput type="checkbox">
+    <PrettyCheck>
+        <!-- Set the FontAwesome check as the default extra -->
+        <template #extra>
+            <slot name="extra">
+                <i class="icon fas fa-check"></i>
+            </slot>
+        </template>
+
+        <!-- Load in all other slots -->
         <template v-for="(_, slot) in $slots" #[slot]="scope">
             <slot :name="slot" v-bind="scope || {}" />
         </template>
-    </PrettyInput>
+    </PrettyCheck>
 </template>
 
 <script setup>
-    import PrettyInput from './pretty_input';
+    import PrettyCheck from 'do-vue/src/templates/pretty-checkbox-vue/pretty_check';
 </script>
--- src/nginxconfig/templates/inputs/radio.vue
+++ src/nginxconfig/templates/inputs/radio.vue
@@ -25,13 +25,21 @@ THE SOFTWARE.
 -->
 
 <template>
-    <PrettyInput type="radio">
+    <PrettyRadio>
+        <!-- Set the FontAwesome check as the default extra -->
+        <template #extra>
+            <slot name="extra">
+                <i class="icon fas fa-check"></i>
+            </slot>
+        </template>
+
+        <!-- Load in all other slots -->
         <template v-for="(_, slot) in $slots" #[slot]="scope">
             <slot :name="slot" v-bind="scope || {}" />
         </template>
-    </PrettyInput>
+    </PrettyRadio>
 </template>
 
 <script setup>
-    import PrettyInput from './pretty_input';
+    import PrettyRadio from 'do-vue/src/templates/pretty-checkbox-vue/pretty_radio';
 </script>
