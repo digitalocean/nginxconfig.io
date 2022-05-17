f5591116077e56b1b1b94009680b58d73901aa6d
kongmoumou <35442047+kongmoumou@users.noreply.github.com>
2022-05-18 05:32:07 +0800

    Fix zhCN translation for fallback routing (#359)

--- src/nginxconfig/i18n/zh-cn/templates/domain_sections/routing.js
+++ src/nginxconfig/i18n/zh-cn/templates/domain_sections/routing.js
@@ -27,8 +27,8 @@ THE SOFTWARE.
 import common from '../../common';
 
 export default {
-    fallbackRouting: '回调路由',
-    fallbackRoutingPhpPath: `回调路由${common.php}路径`,
+    fallbackRouting: '后备路由',
+    fallbackRoutingPhpPath: `后备路由${common.php}路径`,
     legacyPhpRouting: `传统${common.php}路由`,
     enableLegacyRouting: `${common.enable}传统路由`,
     routing: '路由设置',
