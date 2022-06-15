5e840c19dee806f7c4ee850dd2a3c79a62c6a902
Ippei Sumida <ippey.s@gmail.com>
2022-06-15 22:38:39 +0900

    update translations in ja.
    - templates.globalSections.performance.disableHtmlCaching
    - templates.globalSections.performance.enableDisableHtmlCaching

--- src/nginxconfig/i18n/ja/templates/global_sections/performance.js
+++ src/nginxconfig/i18n/ja/templates/global_sections/performance.js
@@ -27,6 +27,8 @@ THE SOFTWARE.
 import common from '../../common';
 
 export default {
+    disableHtmlCaching: 'HTML キャッシュの無効化',
+    enableDisableHtmlCaching: 'TML キャッシュを無効にする',
     gzipCompression: 'Gzip 圧縮',
     enableGzipCompression: `gzip 圧縮を${common.enable}`,
     brotliCompression: 'Brotli 圧縮',
